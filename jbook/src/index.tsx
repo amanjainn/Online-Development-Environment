ab15445023e69a19ea95cff0219f3416ad44e9de
Aman <Aman Jain>
2021-02-12 15:34:14 +0530

    Feature added for error handling in user's code

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,60 +1,77 @@
-import ReactDOM from 'react-dom'
-import {useState,useEffect,useRef} from 'react'
-import * as esbuild from 'esbuild-wasm'
-import {unpkgPathPlugin} from './plugins/unpkg-path-plugins';
-import  {fetchPlugin} from './plugins/fetch-plugin';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
+const App = () => {
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState('');
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-
-
-const App=()=>{
-    const ref=useRef<any>()
-    const [input,setInput]=useState('')
-    const [code,setCode]=useState('')
-
-    const startService = async ()=>{
-        ref.current=  await esbuild.startService({
-            worker:true,
-            wasmURL:'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        })
-        
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
-    useEffect(()=>{
-        startService();
-    },[]);
-    const onClick=async ()=>{
-        if(!ref.current){
-            return;
-        }
-        const result= await ref.current.build({
-           entryPoints:['index.js'],
-           bundle:true,
-           write:false,
-           plugins:[unpkgPathPlugin(),
-           fetchPlugin(input),
-           ],
-           define:{
-               'process.env.NODE_ENV':'"production"',
-               global:'window'
-           }
-        })
-        // console.log(result);
 
-        setCode(result.outputFiles[0].text);
+    iframe.current.srcdoc = html;
 
-    } 
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-    const html=`<script>${code}</script>` //we will send the code to the ifram for eval()
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
 
-    return<div>
-        <textarea value={input} onChange={e=>setInput(e.target.value)}></textarea>
-        <div>
-            <button onClick={onClick} >Submit</button>
-        </div>
-        <pre>{code}</pre>
-        <iframe sandbox="allow-scripts" srcDoc={html}/>
-     </div>
-}
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+    </div>
+  );
+};
 
-ReactDOM.render(<App/>,document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
